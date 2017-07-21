2bcedabef86fc769fc5631dc6ee2eab155a83e4c
NatalijaGucevska <natalija.gucevska@epfl.ch>
2017-07-21 16:02:50 +0200

    Move characterization

---
+++ Move.java
@@ -0,0 +1,22 @@
+public class Move {
+	private int from;
+	private int to; 
+	
+	public Move(int from, int to) {
+		this.from = from; 
+		this.to = to; 
+	}
+	
+	public int getInitialPosition() {
+		return from; 
+	}
+	
+	public int getMovePosition() {
+		return to; 
+	}
+	
+	@Override
+	public String toString() {
+		 return String.format("Moved from %d to %d", getInitialPosition(), getMovePosition()); 
+	}
+}
